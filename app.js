51e1f382b6b092a21561962aab379135eefb5230
Janet Chen <chen@whoop.com>
2016-05-24 16:55:51 -0400

    basic message sending and responding
    API-1317

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-var RtmClient = require('@slack/client').RtmClient;
-
-var token = process.env.SLACK_API_TOKEN || '';
-
-var rtm = new RtmClient(token, {logLevel: 'debug'});
-rtm.start();
-
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const WebSocket = require('ws');
+require('dotenv').load();
+var exp = require('./index.js');
 
 
-var slackClient = new SlackClient("xoxb-45145213361-x918zkl1sGFNlbIBaS5h2FA8")
 
-var bot;
-
-slackClient.on('loggedIn',function(user,team) {
-    bot = user;
-    consold.log("Logged in as " + user.name + " of " +
-    team.name + ", but not yet connected");
-});
-
-slackClient.on('open', function() {
-    console.log('Connected');
-});
-
-slackClient.on('message', function(message) {
-    if (message.user == bot.id) return;
-
-    var channel = slackClient.getChannelGroupOrDMByID(message.channel);
-    channel.send('Hello world!');
-});
+module.exports = {
+    onOpen: onOpen,
+    onMessage: onMessage
+};
--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 const https = require('https');
 const WebSocket = require('ws');
+require('dotenv').load();   // loads variables from .env to ENV
 
+
+// contents of HTTPS server request
 var options = {
     hostname: 'slack.com',
     port: 443,
-    path: '/api/rtm.start?token=xoxb-44894215280-8MGiBbRWEdHI1UdXa097vwtv',
+    path: '/api/rtm.start?token=' + process.env.SLACK_API_TOKEN,
     method: 'GET'
 };
 
-var finishedRequest = () => {
+// when request finished, open WebSocket and handle events
+var finishedRequest = function () {
     var url = JSON.parse(alldata).url;
-    var selfId = JSON.parse(alldata).self.id
+    // var selfId = JSON.parse(alldata).self.id
     // console.log(url);
 
     var socket = new WebSocket(url);
 
-    socket.on('open', () => {
+    // when socket opened, send message
+    var onOpen = socket.on('open', function() {
         console.log('socket opened');
         var msg = {
-            type: "message",
-            text: "WhoopBot connected to WebSocket",
-            id: selfId,
-            date: Date.now()
+            "id": 1,
+            "type": "message",
+            "channel": "C1BBWJ7PF",
+            "text": "WhoopBot connected to WebSocket"
+            // "id": selfId
+
         };
         socket.send(JSON.stringify(msg));
+
     });
+
+    var onMessage = socket.on('message', function(event) {
+        ev = JSON.parse(event);
+        console.log(ev);
+    });
+
 };
 
 var alldata = '';
-var req = https.request(options, (res) => {
+var req = https.request(options, function(res) {
         // console.log('statusCode: ', res.statusCode);
         // console.log('headers: ', res.headers);
 
-        res.on('data', (data) => {
+        res.on('data', function (data) {
             alldata = alldata + data.toString();
             // console.log('end data callback');
         });
@@ -44,3 +58,4 @@ var req = https.request(options, (res) => {
 req.on('close',finishedRequest);
 
 req.end();
+
