bf295df020fb5a0a2b6dfe4a3e5d36b3554881e5
Janet Chen <chen@whoop.com>
2016-06-06 17:15:32 -0400

    Added tests for GitHub getPR function.

--- github.js
+++ github.js
@@ -20,36 +20,48 @@ function getRepos (channel, callback) {
             repo.name + '/pulls'};
 
             // get the pull requests for the repository
-            core.makeRequest(urlOption, function (prArray) {
-
-                // for each pull request per repository, delete if not open
-                prArray.forEach(function (pullrequest) {
-                    if (pullrequest.state !== "open") {
-                        prArray.pop(pullrequest);
-                    }
+            core.makeRequest(urlOption, function(prArray) {
+                var prs = getPR(prArray);
+                callback({
+                    "id": 3,
+                    "type": "message",
+                    "channel": channel,
+                    "text": prs + " open pull request(s) in " + repo.name + " " +
+                    repo.html_url
                 });
-
-                // get number of pull requests in repo
-                var prequests = prArray.length;
-
-                // only send messages for repos with 1+ pull requests
-                if (prequests !== 0) {
-                    //console.log('REPOARRAY[' + index + ']: ', repo);
-                    callback({
-                        "id": 3,
-                        "type": "message",
-                        "channel": channel,
-                        "text": prequests + " open pull request(s) in " +
-                        repo.name
-                    });
-                }
-
-                //console.log('OUTPUT MESSAGE: ', outputMessage);
             });
         });
     });
 }
 
+function getPR (prArray) {
+
+    // for each pull request per repository, delete if not open
+    prArray.forEach(function (pullrequest) {
+        if (pullrequest.state !== "open") {
+            prArray.pop(pullrequest);
+        }
+    });
+
+    // get number of pull requests in repo
+    var prequests = prArray.length;
+
+    // only send messages for repos with 1+ pull requests
+    if (prequests !== 0) {
+
+        return prequests;
+        callback({
+            "id": 3,
+            "type": "message",
+            "channel": channel,
+            "text": prequests + " open pull request(s) in " + repo.name + " " +
+            repo.html_url
+        });
+    }
+
+}
+
 module.exports = {
-    getRepos: getRepos
+    getRepos: getRepos,
+    getPR: getPR
 };
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var index = require('../index.js');
 var app = require('../app.js');
 var httpcat = require('../httpcat.js');
 var core = require('../core.js');
+var github = require('../github.js');
 //var request = require('request');
 //var https = require('https');
 
@@ -87,6 +88,29 @@ describe('handleHTTP', function () {
             done();
         });
 });
+
+var testRepos = [
+                 [{state: "open"},
+                  {state: "open"},
+                  {state: "closed"}],
+                 [{state: "open"}]
+                ];
+
+
+
+describe('getPR', function () {
+  it('should only count open PRs',
+      function (done) {
+        assert.equal(github.getPR(testRepos[0]), "2");
+        done();
+      });
+  it('should work for repos with 1 PR',
+      function (done) {
+        assert.equal(github.getPR(testRepos[1]), "1");
+        done();
+      });
+})
+
 
 
 
