e16be2b1de2dff3514a1312de1d362b07571bbfb
janetchen1 <janet10485@gmail.com>
2016-06-06 14:34:43 -0400

    No longer printing keys (#13)
    please work

--- deploy.bash
+++ deploy.bash
@@ -1,21 +1,11 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-# aws ecs register-task-definition --generate-cli-skeleton > ecs-task.json
+task=$(aws --region=us-west-2 ecs register-task-definition --cli-input-json "$(cat ecs-task.json)" --query=taskDefinition.taskDefinitionArn --output=text)
 
-# aws ecs describe-services --service=ChatOps --query=services[].taskDefintion
+aws --region=us-west-2 ecs update-service --service=ChatOps --desired-count=0
 
-# aws ecs describe-task-definition --task=[output from prev line]
+aws --region=us-west-2 ecs wait services-stable --services=ChatOps
 
-# [copy contents of above into ecs-task.json]
+aws --region=us-west-2 ecs update-service --service=ChatOps --task-definition=$task --desired-count=1
 
-# aws ecs register-task-definition --cli-input-json "$(cat ecs-task.json)"
-
-task=$(aws ecs register-task-definition --cli-input-json "$(cat ecs-task.json)" --query=taskDefinition.taskDefinitionArn --output=text)
-
-aws ecs update-service --service=ChatOps --desired-count=0
-
-aws ecs wait services-stable --services=ChatOps
-
-aws ecs update-service --service=ChatOps --task-definition=$task --desired-count=1
-
-aws ecs wait services-stable --services=ChatOps
+aws --region=us-west-2 ecs wait services-stable --services=ChatOps
