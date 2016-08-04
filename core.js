35377f9f416177607a44825fa79568d21ee5b7d0
Janet Chen <chen@whoop.com>
2016-08-04 13:27:39 -0400

    fix core.makeRequest
    Was redundant before: res.on('end',..) and req.on('end',...) both called the callback function

--- core.js
+++ core.js
@@ -75,27 +75,6 @@ function makeRequest (object, callback, responseCB, postData) {
         req.write(postData);
     }
 
-    req.on('close', function () {
-        console.log('RESPONSE CODE: ' + response.statusCode);
-        //console.log('ACCUMULATOR: ', accumulator);
-
-        // first assume accumulator is JSON object
-        var responseContent;
-        try {
-            responseContent = JSON.parse(accumulator);
-        }
-        catch (err) {
-            responseContent = accumulator;
-        }
-
-        callback(responseContent, response.statusCode);
-
-        if (responseCB) {
-            responseCB(response);
-        }
-
-    });
-
     req.end();
 }
 
